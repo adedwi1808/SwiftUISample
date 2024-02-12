f1995d999a4e06ce8a3b0d1e562e754ba21919f2
Ade Dwi Prayitno <adedwip1808@email.com>
2024-02-12 20:47:59 +0700

    Initial Commit

---
+++ SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by Ade Dwi Prayitno on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
