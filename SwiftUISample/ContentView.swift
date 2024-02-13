5a3099fc68891308af65f5fa15b95803c9f7c98e
Ade Dwi Prayitno <ade@technopartner.co.id>
2024-02-13 08:26:56 +0700

    ref: change Pirate Group Data Model

--- SwiftUISample/ContentView.swift
+++ SwiftUISample/ContentView.swift
@@ -8,20 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    let sampleData: [String: [String]] = [
-        "Straw Hat Pirates": ["Luffy", "Zoro", "Nami", "Sanji", "Chopper", "Robin", "Franky", "Brook", "Jinbe"],
-        "Red Hair Pirates": ["Shanks", "Benn Beckman", "Yasopp", "Lucky Roux", "Rockstar"],
-        "Whitebeard Pirates": ["Edward Newgate", "Marco", "Jozu", "Vista", "Ace", "Thatch", "Blamenco", "Rakuyo", "Namur", "Blenheim", "Curiel", "Haruta", "Atmos", "Fossa", "Izo", "Squard"],
-        "Blackbeard Pirates": ["Marshall D. Teach (Blackbeard)", "Jesus Burgess", "Van Augur", "Doc Q", "Laffitte", "Shiliew", "Sanjuan Wolf", "Vasco Shot", "Catarina Devon", "Avalo Pizarro"]
-    ]
-    
     var body: some View {
-        List {
-            ForEach(Array(sampleData.keys), id: \.self) { pirateGroup in
-                Section(header: Text(pirateGroup)) {
-                    ForEach(sampleData[pirateGroup] ?? [], id: \.self) { pirateName in
-                        Text(pirateName)
-                    }
+        List(sampleData, id: \.self) { pirateGroup in
+            Section(header: Text(pirateGroup.name)) {
+                ForEach(pirateGroup.crew, id: \.self) { pirateName in
+                    Text(pirateName)
                 }
             }
         }
